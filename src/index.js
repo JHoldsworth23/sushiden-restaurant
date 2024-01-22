3e5357c90b8e27a326247a309e6fe5b3c92c915b
JHoldsworth23 <jacobph101@icloud.com>
2024-01-22 21:10:17 +0000

    Debug the h2 to have a closing tag

--- src/index.js
+++ src/index.js
@@ -40,7 +40,7 @@ function bodyComponent() {
     titleCard.innerHTML = `
         <div class='logo'></div>
         <hr>
-        <h2 class="cuisine-title">JAPANESE<h2>
+        <h2 class="cuisine-title">JAPANESE</h2>
         <h1 class="main-title">SUSHI <span class="and-symbol">&</span> POKE</h1>
         <hr>
     `;
