034c0ab8c1e2c3d94fc9d6c042d884cc058bdbd4
JHoldsworth23 <jacobph101@icloud.com>
2024-01-22 18:12:10 +0000

    Create bodyComponent for the main content of the webpage

--- src/index.js
+++ src/index.js
@@ -23,4 +23,27 @@ function navComponent() {
     return content;
 }
 
+function bodyComponent() {
+    const content = document.querySelector('#content');
+    const background = document.createElement('div');
+    background.classList.add('background');
+
+    const titleCard = document.createElement('div');
+    titleCard.classList.add('title-card');
+
+    const line = document.createElement('hr');
+    const title = document.createElement('h1');
+
+    title.textContent = 'Sushi & Poke';
+
+    titleCard.appendChild(line);
+    titleCard.appendChild(title);
+    titleCard.appendChild(line.cloneNode());
+    background.appendChild(titleCard);
+    content.appendChild(background);
+
+    return content;    
+}
+
 document.body.appendChild(navComponent());
+document.body.appendChild(bodyComponent());
