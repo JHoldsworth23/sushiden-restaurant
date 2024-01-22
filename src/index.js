7fa8f0e9839dee4a9d309afc6bdc2f2e7a30918f
JHoldsworth23 <jacobph101@icloud.com>
2024-01-22 17:52:21 +0000

    Create navComponent function for navigation links

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-console.log("It works!");
+function navComponent() {
+    const content = document.querySelector('#content');
+    const linkDiv = document.createElement('div');
+    linkDiv.classList.add('links');
+
+    const aboutUsLink = document.createElement('a');
+    const contactLink = document.createElement('a');
+    const menuLink = document.createElement('a');
+
+    aboutUsLink.textContent = 'About Us';
+    aboutUsLink.href = '#';
+    contactLink.textContent = 'Hours & Locations';
+    contactLink.href = '#';
+    menuLink.textContent = 'Menu';
+    menuLink.href = '#';
+
+    linkDiv.appendChild(aboutUsLink);
+    linkDiv.appendChild(contactLink);
+    linkDiv.appendChild(menuLink);
+
+    content.appendChild(linkDiv);
+
+    return content;
+}
+
+document.body.appendChild(navComponent());
