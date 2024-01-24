d31cbe16c1f2daa0828db4ab55e307a52cf37732
JHoldsworth23 <jacobph101@icloud.com>
2024-01-24 19:11:23 +0000

    Delete navigation component and import navigation function into index.js

--- src/index.js
+++ src/index.js
@@ -1,33 +1,5 @@
+import navComponent from './navigation';
 import './style.css';
-
-function navComponent() {
-    const content = document.querySelector('#content');
-    const linkDiv = document.createElement('div');
-    linkDiv.classList.add('links');
-
-    const aboutUsLink = document.createElement('a');
-    const contactLink = document.createElement('a');
-    const menuLink = document.createElement('a');
-
-    aboutUsLink.classList.add('nav-link');
-    contactLink.classList.add('nav-link');
-    menuLink.classList.add('nav-link');
-
-    aboutUsLink.textContent = 'About Us';
-    aboutUsLink.href = '#';
-    contactLink.textContent = 'Hours & Locations';
-    contactLink.href = '#';
-    menuLink.textContent = 'Menu';
-    menuLink.href = '#';
-
-    linkDiv.appendChild(aboutUsLink);
-    linkDiv.appendChild(contactLink);
-    linkDiv.appendChild(menuLink);
-
-    content.appendChild(linkDiv);
-
-    return content;
-}
 
 function bodyComponent() {
     const content = document.querySelector('#content');
