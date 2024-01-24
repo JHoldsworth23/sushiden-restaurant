d4a1a33f889e4211f1002fa146d8bfca634ffb33
JHoldsworth23 <jacobph101@icloud.com>
2024-01-24 20:04:20 +0000

    Import the bodyComponent from content.js

--- src/index.js
+++ src/index.js
@@ -1,28 +1,7 @@
 import { homeNavComponent, navComponent } from './navigation';
+import bodyComponent from './content';
 import footerComponent from './footer';
 import './style.css';
-
-function bodyComponent() {
-    const content = document.querySelector('#content');
-    const background = document.createElement('div');
-    background.classList.add('background');
-
-    const titleCard = document.createElement('div');
-    titleCard.classList.add('title-card');
-
-    titleCard.innerHTML = `
-        <div class='logo'></div>
-        <hr>
-        <h2 class="cuisine-title">JAPANESE</h2>
-        <h1 class="main-title">SUSHI <span class="and-symbol">& POKE</span></h1>
-        <hr>
-    `;
-
-    background.appendChild(titleCard);
-    content.appendChild(background);
-
-    return content;    
-}
 
 document.body.appendChild(navComponent());
 document.body.appendChild(bodyComponent());
