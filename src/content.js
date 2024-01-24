c8b6112229416ab177d9d86be3bdfb726b0079b3
JHoldsworth23 <jacobph101@icloud.com>
2024-01-24 20:52:31 +0000

    Create a grid layout for location and contact details

--- src/content.js
+++ src/content.js
@@ -1,23 +1,64 @@
-const bodyComponent = () => {
-    const content = document.querySelector('#content');
-    const background = document.createElement('div');
-    background.classList.add('background');
+// const bodyComponent = () => {
+//     const content = document.querySelector('#content');
+//     const background = document.createElement('div');
+//     background.classList.add('background');
 
-    const titleCard = document.createElement('div');
-    titleCard.classList.add('title-card');
+//     const titleCard = document.createElement('div');
+//     titleCard.classList.add('title-card');
 
-    titleCard.innerHTML = `
-        <div class='logo'></div>
-        <hr>
-        <h2 class="cuisine-title">JAPANESE</h2>
-        <h1 class="main-title">SUSHI <span class="and-symbol">& POKE</span></h1>
-        <hr>
+//     titleCard.innerHTML = `
+//         <div class='logo'></div>
+//         <hr>
+//         <h2 class="cuisine-title">JAPANESE</h2>
+//         <h1 class="main-title">SUSHI <span class="and-symbol">& POKE</span></h1>
+//         <hr>
+//     `;
+
+//     background.appendChild(titleCard);
+//     content.appendChild(background);
+
+//     return content;    
+// }
+
+const restaurant = (location, address, country, contact, container) => {
+    const information = document.createElement('div');
+    information.classList.add('grid-item');
+    const [streetName, zipCode, ...name] = address.split(',');
+    const [phoneNo, email] = contact.split(',')
+
+    information.innerHTML = `
+        <h2>${location}</h2>
+        <p>${streetName}</p>
+        <p>${zipCode}</p>
+        <p>${name}</p>
+        <p>${country}</p>
+        <h3>${phoneNo}</h3>
+        <h3>${email}</h3>
     `;
 
-    background.appendChild(titleCard);
-    content.appendChild(background);
-
-    return content;    
+    container.appendChild(information);
 }
 
-export default bodyComponent;
+const locationComponent = () => {
+    const content = document.querySelector('#content');
+    const locationBackground = document.createElement('div');
+    locationBackground.classList.add('location-background');
+
+    const gridContainer = document.createElement('div');
+    gridContainer.classList.add('grid-container');
+
+    const restaurants = [
+        {location: 'London', address: 'Kensington High St, W8 7RG', country: 'United Kingdom', contact: '+44 63421 016174, sushidenuk@email.com'},
+        {location: 'Tokyo', address: 'Shibuya City, Maruyamacho, Building 2nd Floor', country: 'Japan', contact: '+81 82-5238-4234, sushidenjp@email.com'},
+        {location: 'Los Angeles', address: 'Sunset Blvd, CA 90026, California', country: 'United States', contact: '+1 (213) 555-7372, sushidenus@email.com'},
+    ];
+
+    restaurants.forEach((obj) => restaurant(obj.location, obj.address, obj.country, obj.contact, gridContainer));
+
+    locationBackground.appendChild(gridContainer);
+    content.appendChild(locationBackground);
+
+    return content;
+}
+
+export default locationComponent;
