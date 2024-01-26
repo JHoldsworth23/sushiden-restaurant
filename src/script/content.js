5c1996a7e07ff99bd1bd0cf91fdcd7607a57f85e
JHoldsworth23 <jacobph101@icloud.com>
2024-01-26 17:29:37 +0000

    Debug innerHTML div to have a class name

--- src/script/content.js
+++ src/script/content.js
@@ -79,6 +79,7 @@ const menuGrid = (sushi, japanese, description, container) => {
     gridItem.classList.add('menu-grid-item');
 
     gridItem.innerHTML = `
+        <div class="${sushi}-img"></div>
         <h3>${sushi}</h3>
         <p>${japanese}</p>
         <p>${description}</p>
