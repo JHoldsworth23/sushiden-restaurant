4b25120954f9fc89b05a31daa53c60b1fdb9a136
JHoldsworth23 <jacobph101@icloud.com>
2024-01-26 16:22:42 +0000

    Create a function to create grid items for each sushi type

--- src/script/content.js
+++ src/script/content.js
@@ -74,6 +74,19 @@ const locationComponent = () => {
     return content;
 }
 
+const menuGrid = (sushi, japanese, description, container) => {
+    const gridItem = document.createElement('div');
+    gridItem.classList.add('menu-grid-item');
+
+    gridItem.innerHTML = `
+        <h3>${sushi}</h3>
+        <p>${japanese}</p>
+        <p>${description}</p>
+    `;
+
+    container.appendChild(gridItem);
+}
+
 const menuComponent = () => {
     const content = document.querySelector('#content');
     const menuBackground = document.createElement('div');
@@ -92,8 +105,12 @@ const menuComponent = () => {
         {type: 'Temaki', jp: '手巻', description: 'Cold boiled rice moistened with rice vinegar, wrapped together with strips of vegetable or raw fish in a sheet of dried seaweed and rolled into a cone shape.'},
         {type: 'Poke', description: 'Cubes of marinated sushi grade fish which is then tossed over rice and topped with Asian-inspired sauces.'},
         {type: 'Uramaki', jp: '裏巻', description: 'A type of rolled sushi in which the rice is on the outside, rather than rolled up inside the nori.'},
-    ]
+    ];
 
+    menu.forEach(obj => menuGrid(obj.type, obj.jp, obj.description, gridContainer));
+
+    menuBackground.appendChild(title);
+    menuBackground.appendChild(gridContainer);
     content.appendChild(menuBackground);
 
     return content;
