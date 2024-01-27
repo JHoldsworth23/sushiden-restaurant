33c413999b5764c61206ad2998e9ccfe3a07f6d4
JHoldsworth23 <jacobph101@icloud.com>
2024-01-27 14:17:59 +0000

    import initialiseWebpage function from webpage.js

--- src/script/index.js
+++ src/script/index.js
@@ -1,8 +1,3 @@
-import { homeNavComponent, navComponent } from './navigation';
-import { homeComponent, locationComponent, menuComponent } from './content';
-import footerComponent from './footer';
-import '../style.css';
+import initialiseWebpage from "./webpage";
 
-document.body.appendChild(homeNavComponent());
-document.body.appendChild(menuComponent());
-document.body.appendChild(footerComponent());
+initialiseWebpage();
